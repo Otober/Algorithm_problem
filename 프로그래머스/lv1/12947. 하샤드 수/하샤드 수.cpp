111881e3b0f85b4076ca44303f0892ae8dce923d
Otober <45010125+Otober@users.noreply.github.com>
2022-11-08 16:18:12 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    bool answer = false;
+    int temp = x;
+    int sum = 0;
+    while(temp > 0) {
+        sum += temp % 10;
+        temp /= 10;
+    }
+    if(x % sum == 0) answer = true;
+    return answer;
+}
