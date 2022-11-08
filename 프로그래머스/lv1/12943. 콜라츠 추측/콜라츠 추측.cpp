0c2faec03271637fc0562398204080a836f36028
Otober <45010125+Otober@users.noreply.github.com>
2022-11-08 16:18:53 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    int answer = 0;
+    long long n = num;
+    while(n != 1 && answer <= 500) {
+        if(n % 2 == 0) n /= 2;
+        else n = n * 3 + 1;
+        answer++;
+    }
+    if(answer > 500) answer = -1;
+    return answer;
+}
